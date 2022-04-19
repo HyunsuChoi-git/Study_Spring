870b5c937e80c40f5026a9ebf330e139cef95405
HyunsuChoi-git <choihs1054@gmail.com>
2022-04-19 15:45:19 +0900

    #1_addSrcfile

---
+++ src/main/java/com/heracomp/study_Java_oop/Main.java
@@ -0,0 +1,16 @@
+package com.heracomp.study_Java_oop;
+
+
+import com.heracomp.study_Java_oop.logic.BubbleSort;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort<String> sort = new BubbleSort<>();
+
+        System.out.println("[result] "+sort.sort(Arrays.asList(args)));
+
+    }
+}
---
+++ src/test/java/com/heracomp/study_Java_oop/MainTest.java
@@ -0,0 +1,19 @@
+package com.heracomp.study_Java_oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main(){
+
+        //Given
+        String[] args = {"3","1","9","6","2"};
+
+        //When & Then
+        Main.main(args);
+    }
+
+}
